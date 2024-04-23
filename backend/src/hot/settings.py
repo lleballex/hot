767fbaa711e2920a29d29b4082d27e25265153eb
lleballex <lleballex@yandex.ru>
2024-04-23 18:56:05 +0300

    [back] add db, add requirements.txt

--- backend/src/hot/settings.py
+++ backend/src/hot/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'hot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_HOST'),
+        'PORT': getenv('DB_PORT'),
     }
 }
 

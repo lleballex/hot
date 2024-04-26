b14224b3aab5afa13b437cf2a493d97a7df7032b
lleballex <lleballex@yandex.ru>
2024-04-26 20:53:20 +0300

    [front] add react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "@/App.tsx";
 import TelegramProvider from "@/components/special/TelegramProvider";
 import "@/assets/css/index.scss";
@@ -12,10 +14,15 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TelegramProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </TelegramProvider>
   </React.StrictMode>
 );
